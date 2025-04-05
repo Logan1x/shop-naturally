e36fe923f09470218e56caf720b8f185be6c74d8
logan1x <sharmakhushal78@gmail.com>
2025-04-05 20:22:28 +0530

    feat: Add new placeholder text for budget phone search in Bengali

--- static/constants.ts
+++ static/constants.ts
@@ -2,4 +2,5 @@ export const PLACEHOLDER_TEXTS = [
   "show me phone under 20000 rupess with at least 8gb ram",
   "15000 me 256gb storage wale phone batao",
   "10000 में 4GB रैम वाले फोन बताओ",
+  "বাবা-মায়ের জন্য বাজেট ফোন",
 ];

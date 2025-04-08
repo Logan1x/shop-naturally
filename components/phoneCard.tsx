34a368de5eea8e18ebe5132349d3c7a0da49fa53
logan1x <sharmakhushal78@gmail.com>
2025-04-08 19:15:39 +0530

    eslint fix

--- components/phoneCard.tsx
+++ components/phoneCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Star, ExternalLink, ShoppingBag, ArrowUpRight } from "lucide-react";
+import { Star, ShoppingBag, ArrowUpRight } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import { Card } from "@/components/ui/card";
 
